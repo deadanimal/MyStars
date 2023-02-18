2c0c67a48ffaf7d4396fe209550098f07091e82f
Afeez Aziz <afeezaziz@gmail.com>
2023-02-18 12:29:14 +0800

    321

--- resources/views/creator/campaign_detail.blade.php
+++ resources/views/creator/campaign_detail.blade.php
@@ -1,10 +1,13 @@
-@extends('layout')
+@extends('layout-auth')
 
 @section('content')
-    <div class="container is-fluid">
-        <div class="notification is-primary">
-            This container is <strong>fluid</strong>: it will have a 32px gap on either side, on any
-            viewport size.
-        </div>
-    </div>
+    <div class="col-lg-9 pt-4 pb-2 pb-sm-4">
+
+        <h1 class="h2 mb-4">{$campaign->title}</h1>
+
+        <section class="card border-0 py-1 p-md-2 p-xl-3 p-xxl-4 mb-4">
+            <div class="card-body">
+
+            </div>
+        </section>
 @endsection
