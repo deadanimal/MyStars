cd04165ad67b4bd85d10bd8719935e7ff9b3e008
Afeez Aziz <afeezaziz@gmail.com>
2023-04-20 20:42:38 +0800

    123

--- resources/views/challenge/viewform.blade.php
+++ resources/views/challenge/viewform.blade.php
@@ -3,17 +3,12 @@
 @section('content')
     <header class="py-10 ">
         <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 mt-3">
-            <h1 class="text-3xl font-bold tracking-tight">Dashboard</h1>
+            <h1 class="text-3xl font-bold tracking-tight">Challenge</h1>
         </div>
     </header>
 
 
     <main>
-
-        9 new videos to explore <br />
-        recent 3 submissions performance <br />
-        income for the last three months <br />
-
         <div class="flex items-center justify-center">
 
             <div
@@ -57,11 +52,5 @@
                 </form>
             </div>
         </div>
-
-
-
-
-
-
     </main>
 @endsection
